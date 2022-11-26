6e465885b7597fb7f631cf72097519b432cd86e8
Mark Foster <markdouglasfoster@gmail.com>
2022-11-26 15:28:35 -0600

    Add split data function

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -106,3 +106,15 @@ def wrangle_zillow_2():
     df = keep_single_unit_properties(df)
     df = handle_missing_values(df)
     return df
+
+
+def split_data(df, test_size=0.15):
+    '''
+    Takes in a data frame and the train size
+    It returns train, validate , and test data frames
+    with validate being 0.05 bigger than test and train has the rest of the data.
+    '''
+    train, test = train_test_split(df, test_size = test_size , random_state=27)
+    train, validate = train_test_split(train, test_size = (test_size + 0.05)/(1-test_size), random_state=27)
+    
+    return train, validate, test
